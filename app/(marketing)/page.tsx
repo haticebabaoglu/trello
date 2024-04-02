482205814ed71256c29277a282ec3aeafbeb0e25
haticebabaoglu <haticebaba63@gmail.com>
2024-04-02 07:41:02 +0300

    marketing page first update

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,26 @@
+import { Medal } from 'lucide-react'
+import React from 'react'
+
+const MarketingPage = () => {
+  return (
+    <div className='flex items-center justify-center flex-col' > 
+   <div className='flex items-center justify-center flex-col' > 
+   <div className='mb-4 flex items-center border shadow-sm p-4 bg-amber-700 rounded-full uppercase' >
+    <Medal className='h-6 w-6 mr-2' /> 
+    no 1 task managment
+   </div>
+    <h1 className='text-3xl md:text-6xl text- center text-neutral-800 mb-6' >
+    Taskify helps team move 
+    </h1>
+    <div className='text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit'>
+        work forward
+    </div>
+
+    </div>
+      Marketing place
+
+    </div>
+  )
+}
+
+export default MarketingPage
