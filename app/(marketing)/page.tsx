3288a77ab6ba1902191f92f0c72ff585e8dfac83
haticebabaoglu <haticebaba63@gmail.com>
2024-04-03 09:47:11 +0300

    update

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,4 +1,6 @@
+import { Button } from '@/components/ui/button'
 import { Medal } from 'lucide-react'
+import Link from "next/link"
 import React from 'react'
 
 const MarketingPage = () => {
@@ -20,6 +22,11 @@ const MarketingPage = () => {
      <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
      Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique - accomplish it all with Taskify.
      </div>
+     <Button className='mt-6' size="lg" asChild >
+      <Link href="/sign-up">
+      Get Taskify for free 
+      </Link>
+     </Button>
     </div>
   )
 }
