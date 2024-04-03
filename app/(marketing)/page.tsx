ec1f92fad62b9859180dd9f92a9b1605c37cfca1
haticebabaoglu <haticebaba63@gmail.com>
2024-04-03 08:36:20 +0300

    .

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -17,7 +17,9 @@ const MarketingPage = () => {
     </div>
 
     </div>
-    Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique - accomplish it all with Taskify.
+     <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
+     Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique - accomplish it all with Taskify.
+     </div>
     </div>
   )
 }
