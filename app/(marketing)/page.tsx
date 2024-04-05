1ce073b96580123dcb42b1908e6058c83beccb76
haticebabaoglu <haticebaba63@gmail.com>
2024-04-05 10:34:10 +0300

    update

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -43,7 +43,10 @@ const MarketingPage = () => {
     </div>
 
     </div>
-     <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
+    <div className={cn(
+        "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
+        textFont.className,
+      )}>
      Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique - accomplish it all with Taskify.
      </div>
      <Button className='mt-6' size="lg" asChild >
